314b53e24c4b02c51ef6bff55a4b658abe70e65e
RidhimanPatwari <ridhiman.k.patwari@gmail.com>
2021-01-02 18:39:30 -0800

    hi

--- sketch.js
+++ sketch.js
@@ -38,8 +38,8 @@ function setup(){
 
     bird = new Bird(100,100);
 
-    log6 = new Log(230,180,80, PI/2);
-    chain = new Chain(bird.body,log6.body);
+    //log6 = new Log(230,180,80, PI/2);
+    slingshot = new Slingshot(bird.body,{x:200, y:200});
 }
 
 function draw(){
@@ -48,7 +48,7 @@ function draw(){
     strokeWeight(4);
     box1.display();
     box2.display();
-    ground.display();
+ 
     pig1.display();
     log1.display();
 
@@ -61,8 +61,21 @@ function draw(){
     log4.display();
     log5.display();
 
+    slingshot.display(); 
     bird.display();
+   //noStroke();
     platform.display();
-    log6.display();
-    chain.display();    
+    ground.display();
+    //log6.display();
+       
+}
+
+function mouseDragged(){ 
+
+    Matter.Body.setPosition(bird.body, {x: mouseX, y: mouseY}); 
+}
+
+function mouseReleased(){ 
+
+    slingshot.fly(); 
 }
